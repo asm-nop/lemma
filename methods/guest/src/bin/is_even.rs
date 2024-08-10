d2cacbfebd980a2dbae8e39bbd6ea1c2ba113078
Eric Woolsey <ewoolsey@ualberta.ca>
2024-08-10 13:12:28 -0400

    circuit

--- methods/guest/src/bin/is_even.rs
+++ methods/guest/src/bin/is_even.rs
@@ -16,26 +16,51 @@ use std::io::Read;
 
 use alloy_primitives::U256;
 use alloy_sol_types::SolValue;
-use risc0_zkvm::guest::env;
+use serde::{Serialize, Deserialize};
+use risc0_zkvm::sha::Digest;
 
-use parser::command::{parse, Command};
-use proost::evaluator::Evaluator;
+use risc0_zkvm::{
+    guest::env,
+    sha::{Impl, Sha256},
+};
+
+// use parser::command::{parse, Command};
+// use proost::evaluator::Evaluator;
+
+#[derive(Serialize, Deserialize)]
+pub struct Inputs {
+    theorem_template: String,
+    solution: String,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct Outputs {
+    solution_hash: Digest,
+}
 
 fn main() {
     // Read the input data for this application.
-    let mut input_bytes = Vec::<u8>::new();
-    env::stdin().read_to_end(&mut input_bytes).unwrap();
-    // Decode and parse the input
-    let number = <U256>::abi_decode(&input_bytes, true).unwrap();
+    // let mut input_bytes = Vec::<u8>::new();
+    // env::stdin().read_to_end(&mut input_bytes).unwrap();
+    // // Decode and parse the input
+    // let number = <U256>::abi_decode(&input_bytes, true).unwrap();
+    let inputs: Inputs = env::read();
+    
+    let statement = inputs.theorem_template + &inputs.solution;
+    
+    let sha = *Impl::hash_bytes(&statement.as_bytes());
 
     // Run the computation.
     // In this case, asserting that the provided number is even.
-    assert!(!number.bit(0), "number is not even");
-    let mdln = include_str!("../../../../mdln/examples/eq.mdln");
+    assert!(true, "number is not even");
+    // let mdln = include_str!("../../../../mdln/examples/eq.mdln");
     //let command = parse::file(mdln);
     //let evaluator = Evaluator::new();
 
     // Commit the journal that will be received by the application contract.
     // Journal is encoded using Solidity ABI for easy decoding in the app contract.
-    env::commit_slice(number.abi_encode().as_slice());
+    let outputs = Outputs {
+        solution_hash: sha,
+    };
+    env::commit(&outputs);
 }
